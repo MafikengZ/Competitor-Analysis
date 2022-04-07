9eb26395a811dd4c783ffc369cb40ebe6679b5f4
MfundoMhlanga <84128860+MfundoMhlanga@users.noreply.github.com>
2022-04-07 21:59:49 +0200

    Add files via upload

---
+++ Competitor.sql
@@ -0,0 +1,211 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: competitor_analysis
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `competitors`
+--
+
+DROP TABLE IF EXISTS `competitors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `competitors` (
+  `competitorsID` int NOT NULL AUTO_INCREMENT,
+  `competitor_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`competitorsID`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `competitors`
+--
+
+LOCK TABLES `competitors` WRITE;
+/*!40000 ALTER TABLE `competitors` DISABLE KEYS */;
+INSERT INTO `competitors` VALUES (1,'Hyperndev'),(2,'IT Academy'),(3,'Africa Data School'),(4,'Moringa School'),(5,'Simplilearn'),(6,'GetSmarter'),(7,'2u'),(8,'Data Science Dojo'),(9,'Udecity'),(10,'NYC Data Science Academy'),(11,'Coursera');
+/*!40000 ALTER TABLE `competitors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `main_table`
+--
+
+DROP TABLE IF EXISTS `main_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `main_table` (
+  `main_tableID` int NOT NULL AUTO_INCREMENT,
+  `yearID` int NOT NULL,
+  `monthID` int NOT NULL,
+  `post_typeID` int NOT NULL,
+  `reactionsID` int NOT NULL,
+  `social_mediaID` int NOT NULL,
+  `competitorID` int NOT NULL,
+  PRIMARY KEY (`main_tableID`),
+  KEY `yearID_idx` (`yearID`),
+  KEY `monthID_idx` (`monthID`),
+  KEY `post_typeID_idx` (`post_typeID`),
+  KEY `reactionsID_idx` (`reactionsID`),
+  KEY `social_mediaID_idx` (`social_mediaID`),
+  KEY `competitorID_idx` (`competitorID`),
+  CONSTRAINT `competitorID` FOREIGN KEY (`competitorID`) REFERENCES `competitors` (`competitorsID`),
+  CONSTRAINT `monthID` FOREIGN KEY (`monthID`) REFERENCES `month` (`monthID`),
+  CONSTRAINT `post_typeID` FOREIGN KEY (`post_typeID`) REFERENCES `post_type` (`post_typeID`),
+  CONSTRAINT `reactionsID` FOREIGN KEY (`reactionsID`) REFERENCES `reactions` (`reactionsID`),
+  CONSTRAINT `social_mediaID` FOREIGN KEY (`social_mediaID`) REFERENCES `social_media` (`social_mediaID`),
+  CONSTRAINT `yearID` FOREIGN KEY (`yearID`) REFERENCES `year` (`yearID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `main_table`
+--
+
+LOCK TABLES `main_table` WRITE;
+/*!40000 ALTER TABLE `main_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `main_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `month`
+--
+
+DROP TABLE IF EXISTS `month`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `month` (
+  `monthID` int NOT NULL AUTO_INCREMENT,
+  `Month` varchar(45) NOT NULL,
+  PRIMARY KEY (`monthID`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `month`
+--
+
+LOCK TABLES `month` WRITE;
+/*!40000 ALTER TABLE `month` DISABLE KEYS */;
+INSERT INTO `month` VALUES (1,'January'),(2,'Feburary'),(3,'March'),(4,'April'),(5,'May'),(6,'June'),(7,'July'),(8,'August'),(9,'September'),(10,'October'),(11,'November'),(12,'December');
+/*!40000 ALTER TABLE `month` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post_type`
+--
+
+DROP TABLE IF EXISTS `post_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post_type` (
+  `post_typeID` int NOT NULL AUTO_INCREMENT,
+  `media` varchar(45) NOT NULL,
+  PRIMARY KEY (`post_typeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post_type`
+--
+
+LOCK TABLES `post_type` WRITE;
+/*!40000 ALTER TABLE `post_type` DISABLE KEYS */;
+INSERT INTO `post_type` VALUES (1,'Text'),(2,'Image'),(3,'Video');
+/*!40000 ALTER TABLE `post_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reactions`
+--
+
+DROP TABLE IF EXISTS `reactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reactions` (
+  `reactionsID` int NOT NULL AUTO_INCREMENT,
+  `num_comments` int DEFAULT NULL,
+  `num_reactions` int DEFAULT NULL,
+  PRIMARY KEY (`reactionsID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reactions`
+--
+
+LOCK TABLES `reactions` WRITE;
+/*!40000 ALTER TABLE `reactions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `social_media`
+--
+
+DROP TABLE IF EXISTS `social_media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `social_media` (
+  `social_mediaID` int NOT NULL AUTO_INCREMENT,
+  `Type_of_social_media` varchar(45) NOT NULL,
+  PRIMARY KEY (`social_mediaID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `social_media`
+--
+
+LOCK TABLES `social_media` WRITE;
+/*!40000 ALTER TABLE `social_media` DISABLE KEYS */;
+INSERT INTO `social_media` VALUES (1,'Facebook'),(2,'LinkedIN'),(3,'Twitter'),(4,'Instagram'),(5,'YouTube');
+/*!40000 ALTER TABLE `social_media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `year`
+--
+
+DROP TABLE IF EXISTS `year`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `year` (
+  `yearID` int NOT NULL AUTO_INCREMENT,
+  `year` int NOT NULL,
+  PRIMARY KEY (`yearID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `year`
+--
+
+LOCK TABLES `year` WRITE;
+/*!40000 ALTER TABLE `year` DISABLE KEYS */;
+INSERT INTO `year` VALUES (1,2020),(2,2021);
+/*!40000 ALTER TABLE `year` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-07 21:58:48
