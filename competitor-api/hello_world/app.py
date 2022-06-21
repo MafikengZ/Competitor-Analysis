eb5793ef72501db936e189a315c729916d1bf959
Sello Sydney Mafikeng <tebogomafikeng@gmail.com>
2022-06-21 15:21:32 +0200

    Refactored __init__.py file

--- competitor-api/hello_world/app.py
+++ competitor-api/hello_world/app.py
@@ -1,7 +1,4 @@
-from utils.twitter import _load_twitter_data , _tweet_preprocessor
-from utils.twitter import _preprocess_dataset , _store_dataset
-from utils.facebook import _load_facebook_data , _store_dataset
-from utils.facebook import _preprocess_data
+from utils import *
 
 
 if __name__ == '__main__':
--- competitor-api/hello_world/utils/__init__.py
+++ competitor-api/hello_world/utils/__init__.py
@@ -1,4 +1,1 @@
-from .utils.twitter import _load_twitter_data , _tweet_preprocessor
-from .utils.twitter import _preprocess_dataset , _store_dataset
-from .utils.facebook import _load_facebook_data , _store_dataset
-from .utils.facebook import _preprocess_data
+__all__ =['facebook' , 'twitter' ,'instagram' , 'linkedin' , 'youtube']
